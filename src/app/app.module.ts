cd69c622d535a4b7728b3ea9773bf221ced7997d
Naveen8787 <61699352+Naveen8787@users.noreply.github.com>
2020-04-20 23:29:23 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BioComponent } from './bio/bio.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillComponent } from './skill/skill.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BioComponent,
+    PortfolioComponent,
+    SkillComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
